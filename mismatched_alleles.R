5f7c28498b4244592a67767ddb60cc501974267b
Bonnie Lin <b2lin@ucsd.edu>
2020-06-01 13:12:22 -0700

    create graphic for plotting the snps along the chromosomes for subset of unassinged snps (neither ref nor alt match reference)

--- mismatched_alleles.R
+++ mismatched_alleles.R
@@ -23,7 +23,23 @@ unassigned_rsid_err_vars_freq %>%
   labs(title = "Frequency distribution of Subset of Unassigned SNPs (Neither REF nor ALT match reference)",
        x = "Allele Frequency")
 # if we only have minor alleles, than we will only have frequency from 0 to 0.5, so don't be alarmed if this is the case
-#(2) map their positions (are they concentrated in some places or uniformly distributed over the genome?). One way to do this is to calculate distance between neigbouring alleles and plot distribution of thiese distances. They other way is to plot them along the chromosomes.
+
+#(2) map their positions 
+# (are they concentrated in some places or uniformly distributed over the genome?). 
+# One way to do this is to calculate distance between neigbouring alleles and plot distribution of these distances. 
+# unassigned_rsid_err_vars_freq %>% 
+  
+# The other way is to plot them along the chromosomes.
+unassigned_rsid_err_vars_freq %>%
+  mutate_at(vars("POS"), as.numeric) %>% 
+  mutate(CHROM = factor(CHROM, levels = 1:20)) %>% 
+  ggplot(aes(x = CHROM, y = POS)) +
+  geom_point(size = 1) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+  scale_y_continuous(labels = scales::comma,
+                     breaks = scales::pretty_breaks(n = 10)) +
+  labs(title = "Chromosome Positions of the Subset of Unassigned SNPs (Neither REF nor ALT match reference)")
+  
 
 
 #(3) check where we got those allele from (which set of variants - Jun Li? HS from 42 strains?) The way to do this is to see how many of those variants overlap with our reference datasets.
