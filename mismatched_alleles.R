f3329eccfce503890ba777366e7d52e428a27f85
Bonnie Lin <b2lin@ucsd.edu>
2020-06-01 17:07:34 -0700

    complete check where we got the alleles from

--- mismatched_alleles.R
+++ mismatched_alleles.R
@@ -27,8 +27,10 @@ unassigned_rsid_err_vars_freq %>%
 #(2) map their positions 
 # (are they concentrated in some places or uniformly distributed over the genome?). 
 # One way to do this is to calculate distance between neigbouring alleles and plot distribution of these distances. 
-# unassigned_rsid_err_vars_freq %>% 
-  
+unassigned_rsid_err_vars_freq %>% 
+  select()
+  group
+    
 # The other way is to plot them along the chromosomes.
 unassigned_rsid_err_vars_freq %>%
   mutate_at(vars("POS"), as.numeric) %>% 
@@ -43,4 +45,22 @@ unassigned_rsid_err_vars_freq %>%
 
 
 #(3) check where we got those allele from (which set of variants - Jun Li? HS from 42 strains?) The way to do this is to see how many of those variants overlap with our reference datasets.
+# might delete these commented lines later
+# founder_seq = as.data.frame(do.call(rbind, strsplit(readLines("founder_GT_transposedPED.txt"), split=" {2,10}")))
+# paste0("-h ","/oasis/tscc/scratch/aschitre/round8_imputation/reference_files/refs_impute2/42genomes_homozygous_",chr,"QC.ref.impute.hap.gz",newline_command)
 
+# in tscc load module R
+impute_legend_files <- list.files(pattern = ".*legend.gz")
+genomes42 <- lapply(impute_legend_files, function(x){data.table::fread(paste0("zcat ", "'", x,"'", " | awk \'{print $1}\'"), fill = T)})
+genomes42_df <- data.table::rbindlist(genomes42)
+save(genomes42_df, file = "chr_pos_all.RData") # transfer from tscc into filezilla
+# back here
+load("~/Dropbox (Palmer Lab)/Palmer Lab/Bonnie Lin/P50/SNPs/chr_pos_all.RData")
+unassigned_rsid_data_genomes42 <- unassigned_rsid_err_vars %>% 
+  left_join(., genomes42_df %>% 
+  separate(id, into = c("chr", "pos"), sep = ":") %>% 
+  mutate(chr = parse_number(chr) %>% as.character) %>% 
+  mutate(genomes_42 = "1"), by = c("CHROM" = "chr", 
+                                   "POS" = "pos")) %>% 
+  mutate(genomes_42 = ifelse(is.na(genomes_42), "0", genomes_42))
+unassigned_rsid_data_genomes42 %>% select(genomes_42) %>% table()
